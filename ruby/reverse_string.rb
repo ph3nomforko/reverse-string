db5990897e2a1c81e527bb6ae81dd6c798be9d04
Codi Kraus <codi.kraus@gmail.com>
2021-11-30 10:12:14 -0600

    Done

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,12 +1,17 @@
 def reverse_string(str)
   # type your code in here
+  new_array = str.split('')
+  reverse_array = []
+  new_array.map do { |e| reverse_array.unshift(e) }
+  return reverse_array.join
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
   puts "=>", reverse_string('hi')
 
-  puts
+  puts "Expecting: 'godym'"
+  puts "=>", reverse_string('mydog')
 
   puts "Expecting: 'ybabtac'"
   puts "=>", reverse_string('catbaby')
@@ -16,3 +21,6 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+# str.split
+# map over string_array by unshifting into new aray
+# join reverse array into string
