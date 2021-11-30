68e848086e00ea3f48c1e8a4298395396636f268
Codi Kraus <codi.kraus@gmail.com>
2021-11-30 10:11:49 -0600

    All tests passing

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
   // type your code here
+  let strArray = str.split('')
+  const reverseArray = []
+  strArray.map(e => reverseArray.unshift(e))
+  let reverseString = reverseArray.join('')
+  return reverseString
 }
 
 if (require.main === module) {
